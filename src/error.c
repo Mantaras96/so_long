8d067af23d801dfd6571cc1f5aaad3faae7f6dce
Albert <albert.mantaras@gmail.com>
2022-03-14 23:26:09 +0100

    Modificaciones de estructura y main

---
+++ src/error.c
@@ -0,0 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albertmantaras <albertmantaras@student.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/14 23:23:18 by albertmanta       #+#    #+#             */
+/*   Updated: 2022/03/14 23:25:19 by albertmanta      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+void error_parameters(){
+    ft_putstr_fd('Numero de parametros incorrecto', 1);
+}
